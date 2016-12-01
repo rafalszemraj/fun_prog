729be60d4865fc56119e6ca0eaec6cab1a0bef20
Rafał Szemraj <rafael.szemraj@gmail.com>
2016-12-01 12:09:39 +0100

    Update transactions_generator.js

--- transactions_generator.js
+++ transactions_generator.js
@@ -2,40 +2,38 @@ const fs = require('fs');
 const _ = require('ramda');
 const chance = new (require('chance'));
 
-const fxs = {};
+const cache = {};
 
-const calculateCCyFX = ccy => ccy === 'USD' ? 1 : (fxs[ccy] ? fxs[ccy] : fxs[ccy] = chance.floating({min:.5, max:1.5}) );
+const fromCache = (key, newValueFactory) => cache[key] ? cache[key] : cache[key] = newValueFactory();
+
+const getRic = () => chance.pickone(['APPLE.OQ', 'VOD.L', 'WMT.N', 'IBM.N']);
+const getCcy  = () => chance.pickone(['USD', 'EUR', 'GBP', 'PLN', 'AUS', 'JPN', 'HKG']);
+const getShares = () => chance.natural({ min: 300, max: 500 });
+const calculateCCyFX = ccy => fromCache(ccy, () => ccy === 'USD' ? 1 : chance.floating({min:.5, max:1.5}) );
+const calculateValue = ric => fromCache(ric, () => chance.natural({min: 300, max: 500}) );
+const getTrader = () => chance.pickone(['Mike', 'Steven', 'John', 'Bill']);
+
 
 chance.mixin({
-  "transaction": () => (
-    {
-      ccy: chance.pickone(['USD', 'EUR', 'GBP', 'PLN', 'AUS', 'JPN', 'HKG']),
-      ric: chance.pickone(['APPLE.OQ', 'VOD.L', 'WMT.N', 'IBM.N']),
-      value: chance.natural({ min: 200, max: 9000 }),
-      shares: chance.natural({ min: 300, max: 500 })
-    }
-  ),
-  "transaction_fx": () => {
+  "transaction": () => {
 
-    const ccy = chance.pickone(['USD', 'EUR', 'GBP', 'PLN', 'AUS', 'JPN', 'HKG']);
-    const fx = calculateCCyFX(ccy)
+    const ccy = getCcy();
+    const ric = getRic();
     return {
       ccy,
-      ric: chance.pickone(['APPLE.OQ', 'VOD.L', 'WMT.N', 'IBM.N']),
-      value: chance.natural({ min: 200, max: 9000 }),
-      shares: chance.natural({ min: 300, max: 500 }),
-      fx
-    }
-
+      ric,
+      value:calculateValue(ric),
+      shares:getShares(),
+      /*trader:getTrader(),*/
+      /*fx: calculateCCyFX(ccy)*/}
   }
-
 })
 
-const transaction = chance.transaction,
-      transaction_fx = chance.transaction_fx;
+
 
 try {
   fs.mkdirSync(__dirname + "/dist");
 }
 catch(e) {}
-fs.writeFileSync(__dirname + "/dist/transactions_data.json", JSON.stringify(_.times(transaction_fx, 100)));
+fs.writeFileSync(__dirname + "/dist/transactions_data.json", JSON.stringify(_.times(chance.transaction, 100)));
+
