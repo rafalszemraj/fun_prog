baca71ce1237329d393f96c7bd7eea32a3257483
Rafał Szemraj <rafael.szemraj@gmail.com>
2016-12-02 11:14:46 +0100

    Update transactions.js

--- transactions.js
+++ transactions.js
@@ -1,47 +1,44 @@
 const data = require('json-loader!./dist/transactions_data.json');
 const _ = require('ramda'),
-      generateChart = require('./chart');
+    generateChart = require('./chart');
 const compose = _.compose,
-      curry = _.curry,
-      sum = _.sum,
-      map = _.map,
-      groupBy = _.groupBy,
-      mean = _.mean,
-      applySpec = _.applySpec,
-      values = _.values,
-      prop = _.prop,
-      length = _.length;
+    curry = _.curry,
+    sum = _.sum,
+    map = _.map,
+    groupBy = _.groupBy,
+    mean = _.mean,
+    applySpec = _.applySpec,
+    values = _.values,
+    prop = _.prop,
+    length = _.length;
 
 // [Transaction] -> {String:[Transaction]}
 const groupTransactionsBy = groupBy;
 
 // [Transaction] -> Number
-const totalOf = of => compose( sum, map(prop(of)));
+const totalOf = of => compose(sum, map(prop(of)));
 const avgOf = of => compose(mean, map(prop(of)));
 
 // build filter data
-const transactionListStats = applySpec(
+const transactionListStats = transactions => (
 {
-  ['total shares']: totalOf('shares'),
-  ['total value']: totalOf('value')
+    ['total shares']: totalOf('shares')(transactions),
+    ['total value']: totalOf('value')(transactions)
 }
-)
+);
 
 // [Transaction] -> {by:[transform(Transaction)}
-const createBreakdownBy = curry((by, transform) => compose( map(transform), groupTransactionsBy(prop(by))));
+const createBreakdownBy = curry((transform, by) => compose(map(transform), groupTransactionsBy(prop(by))));
 
-const ccyBreakdown = createBreakdownBy('ccy');
-const ricBreakdown = createBreakdownBy('ric');
+//const createChartBreakdown = by => createBreakdownBy(transactionListStats, by);
 
-const createChartForBreakdown = (container, breakdown) => compose( generateChart(container), breakdown(transactionListStats) );
+generateChart(createBreakdownBy(transactionListStats, 'ccy')(data));
+generateChart(createBreakdownBy(transactionListStats, 'ric')(data));
 
-createChartForBreakdown('#chart1', ccyBreakdown)(data);
-createChartForBreakdown('#chart2', ricBreakdown)(data);
 
-//
-// console.log( ccyBreakdown(length)(data));
-// console.log( ccyBreakdown(avgOf('shares'))(data));
-// console.log( ccyBreakdown(avgOf('value'))(data));
+// console.log( ccyBreakDown(length)(data));
+// console.log( ccyBreakDown(avgOf('shares'))(data));
+// console.log( ccyBreakDown(avgOf('value'))(data));
 //
 // console.log( ricBreakdown(length)(data));
 // console.log( ricBreakdown(avgOf('shares'))(data));
